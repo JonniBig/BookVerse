000060de8fbb2944b5ff98f929fd40d597e0ec50
JonniBig <bigun.i88@gmail.com>
2023-08-31 16:09:44 +0300

    project-markup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './js/categories';
 import './js/foundation';
 import './js/bestSellers';
 import './js/mobileMenu';
 import './js/modalWindow';
 import './js/toogleBtn';
+import './js/categories';
