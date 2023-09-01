8d0cb7d933a0c8fa420150823db8fd30c03f8c98
Alexandr150983 <alexandr150983@gmail.com>
2023-09-01 10:06:57 +0300

    renderCategories

--- src/js/categories.js
+++ src/js/categories.js
@@ -18,4 +18,26 @@ function fetchCategories() {
     });
 }
 
+function renderCategories(categories) {
+  categoriesContainer.innerHTML = '';
+
+  const categoryList = document.createElement('ul');
+  categoryList.classList.add('book_list');
+
+  categories.forEach(category => {
+    const categoryItem = document.createElement('li');
+    const categoryLink = document.createElement('a');
+
+    categoryItem.classList.add('category_item');
+    categoryLink.classList.add('category_link');
+    categoryLink.textContent = category.list_name;
+    categoryLink.href = '#'; // посилання на обробник події
+
+    categoryItem.appendChild(categoryLink);
+    categoryList.appendChild(categoryItem);
+  });
+
+  categoriesContainer.appendChild(categoryList);
+}
+
 fetchCategories();
