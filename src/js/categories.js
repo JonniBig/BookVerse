6fdeb21472c7396fb837848f1ddb2a771d922b0b
Alexandr150983 <alexandr150983@gmail.com>
2023-09-01 09:37:15 +0300

    fetchCategories

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,1 +1,21 @@
+import axios from 'axios';
 
+const BASE_URL = 'https://books-backend.p.goit.global/';
+
+const categoriesContainer = document.querySelector('.categories');
+
+function fetchCategories() {
+  axios
+    .get(`${BASE_URL}books/category-list`)
+    .then(response => {
+      const categories = response.data;
+      const categoriesAll = { list_name: 'All categories' };
+      categories.unshift(categoriesAll);
+      renderCategories(categories);
+    })
+    .catch(error => {
+      console.error('Помилка при отриманні категорій:', error);
+    });
+}
+
+fetchCategories();
