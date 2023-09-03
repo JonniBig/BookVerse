797a8cc4a7e75b2ac48a54c67412daf43ace2ade
KolosIryna <kolos.irina23@gmail.com>
2023-09-03 14:40:20 +0300

    header-mobmenu

--- src/js/mobileMenuBurger.js
+++ src/js/mobileMenuBurger.js
@@ -24,18 +24,44 @@ window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
   bodyScrollLock.enableBodyScroll(document.body);
 });
 
-// Переключання стилю на активних сторінках
+// Переключання стилю на активних сторінках мобільного меню
 document.addEventListener('DOMContentLoaded', function () {
-  var currentPage = window.location.pathname;
-
-  var menuItemHome = document.querySelector('.mobil-nav-item-home');
-  var menuItemShopping = document.querySelector('.mobil-nav-item-shopping');
-  var iconShoppingBag = document.querySelector('.mobil-lock');
+  const currentPage = window.location.pathname;
+  const menuItemHome = document.querySelector('.mobil-nav-item-home');
+  const menuItemShopping = document.querySelector('.mobil-nav-item-shopping');
+  const iconShoppingBag = document.querySelector('.mobil-lock');
 
   if (currentPage.includes('index.html')) {
     menuItemHome.classList.add('mobil-nav-item-active');
-    iconShoppingBag.classList.add('mobil-lock-active');
+    menuItemShopping.classList.remove('mobil-nav-item-active'); // Знімаємо активний стиль з "Shopping list"
+    iconShoppingBag.classList.remove('mobil-lock-active'); // Знімаємо активний стиль для іконки
   } else if (currentPage.includes('shopping-list.html')) {
     menuItemShopping.classList.add('mobil-nav-item-active');
+    menuItemHome.classList.remove('mobil-nav-item-active'); // Знімаємо активний стиль з "Home"
+    iconShoppingBag.classList.add('mobil-lock-active'); // Додаємо активний стиль для іконки
   }
+});
+
+// Переключання стилю на активних сторінках головної сторінки
+document.addEventListener('DOMContentLoaded', function () {
+  const currentPage = window.location.pathname;
+  const navItemHome = document.querySelector('.nav-item-home');
+  const navItemShopping = document.querySelector('.nav-item-shopping');
+  const navLockIcon = document.querySelector('.nav-lock');
+
+  function setActiveMenuItem(currentPage) {
+    if (currentPage.includes('index.html')) {
+      // Якщо поточна сторінка - Home
+      navItemHome.classList.add('nav-item-active');
+      navItemShopping.classList.remove('nav-lock-active');
+      navLockIcon.classList.remove('nav-lock-active');
+    } else if (currentPage.includes('shopping-list.html')) {
+      // Якщо поточна сторінка - Shopping List
+      navItemShopping.classList.add('nav-item-active');
+      navItemHome.classList.remove('nav-item-active');
+      navLockIcon.classList.adde('nav-lock-active');
+    }
+  }
+
+  setActiveMenuItem(currentPage);
 });
