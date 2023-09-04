d663ae31c2deab5bf29c5f8b7a9c85e3874d1f20
KolosIryna <kolos.irina23@gmail.com>
2023-09-04 22:35:07 +0300

    reset

--- src/js/foundation.js
+++ src/js/foundation.js
@@ -1,56 +1,48 @@
-
-
-const isLocalhost =
-  window.location.hostname === 'localhost' ||
-  window.location.hostname === '127.0.0.1';
-
 const foundations = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: isLocalhost
-      ? '/images/found/children.png'
-      : 'https://jonnibig.github.io/BookVerse/images/found/children.png',
+    img: 'children.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: 'https://jonnibig.github.io/BookVerse/images/found/hope.png',
+    img: 'hope.png',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: 'https://jonnibig.github.io/BookVerse/images/found/medical.png',
+    img: 'medical.png',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: 'https://jonnibig.github.io/BookVerse/images/found/razom.png',
+    img: 'razom.png',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: 'https://jonnibig.github.io/BookVerse/images/found/action.png',
+    img: 'action.png',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: 'https://jonnibig.github.io/BookVerse/images/found/sergiy.png',
+    img: 'sergiy.png',
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: 'https://jonnibig.github.io/BookVerse/images/found/medecins.png',
+    img: 'medecins.png',
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: 'https://jonnibig.github.io/BookVerse/images/found/world.png',
+    img: 'world.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: 'https://jonnibig.github.io/BookVerse/images/found/united.png',
+    img: 'united.png',
   },
 ];
 
@@ -85,10 +77,10 @@ function createMarkupListItem(item, i) {
         <a href="${item.url}" class="found-link" title="${
     item.title
   }" target="_blank"> 
-
-                <img src="${item.img}" alt="${item.title}" class="found-logo">
-            </a>
-        </li>
-    `;
-
+            <img src="./images/found/${item.img}" alt="${
+    item.title
+  }" class="found-logo">
+        </a>
+    </li>
+`;
 }
