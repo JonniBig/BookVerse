97c5555eecd05b20a5393f36deaf1fdca4054726
ViNezh <126874232+ViNezh@users.noreply.github.com>
2023-09-05 19:09:29 +0300

    with out pagination

--- src/js/shopingList.js
+++ src/js/shopingList.js
@@ -1,49 +1,60 @@
-import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
+// import axios from 'axios';
+// import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
 
 const listCards = document.querySelector('.book-shop-list');
 const emptyList = document.querySelector('.empty-list');
 const container = document.querySelector('.tui-pagination');
 
-let savedData = JSON.parse(localStorage.getItem('ListOfBooks'));
-const itemsPerPage = 3;
-let currentPage = 1;
-addCard(currentPage);
+// async function getBook() {
+//   const url = 'https://books-backend.p.goit.global/books/top-books';
+//   try {
+//     // Запит на бекенд за допомогою axios
+//     const getData = await axios.get(`${url}`);
+//     return getData.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-function addCard(page) {
-    if (savedData.length === 0) {
-    chekIfEmpty();
+// getBook().then(data => {
+//   localStorage.removeItem('ListofBooks');
+//   const savedBook = data[15].books;
+//   localStorage.setItem('ListOfBooks', JSON.stringify(savedBook));
+// });
+
+  const savedData = JSON.parse(localStorage.getItem('ListOfBooks'));
+addCard();
+
+function addCard() {
+
+  if (savedData.length === 0) {
+    emptyList.classList.remove('is-hidden');
+    listCards.innerHTML = '';
+    listCards.classList.add('is-hidden');
     return;
   }
-  prepareForMarkup();
-  const startIndex = (page - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const pageForBooks = savedData.slice(startIndex, endIndex);
-  // 
-  if (pageForBooks.length === 0) {
-    location.reload();
- }
-  listCards.insertAdjacentHTML('beforeend', makeMarkup(pageForBooks));
+  listCards.classList.remove('is-hidden');
+  emptyList.classList.add('is-hidden');
+  listCards.innerHTML = '';
+  listCards.insertAdjacentHTML('beforeend', makeMarkup11(savedData));
+  // listCards.firstChild.classList.add('tui-first-child');
+  // listCards.lastChild.classList.add('tui-last-child');
+  // const options = {
+  //   // totalItems: 5,
+  //   itemsPerPage: 3,
+  //   visiblePages: 2,
+  //   page: 1,
+  //   centerAlign: true,
+  //   firstItemClassName: 'tui-first-child',
+  //   lastItemClassName: 'tui-last-child',
+  // };
 
-
-  // -----------Накладаємо слухачі на кнопку видалення картки зі сховища
+  // const myPagination = new Pagination(container, options);
   const dumpEl = document
     .querySelectorAll('.icon-damp')
     .forEach(item => item.addEventListener('click', deleteCard));
 }
-
-  const options = {
-    totalItems: savedData.length,
-    itemsPerPage,
-    visiblePages: 2,
-    centerAlign: true,
-  };
-  const myPagination = new Pagination(container, options);
-
-myPagination.on('beforeMove', eventData => {
-  currentPage = eventData.page;
-  addCard(currentPage);
-});
 
 function deleteCard(evt) {
   evt.preventDefault();
@@ -52,18 +63,18 @@ function deleteCard(evt) {
   savedData.splice(cardIndex, 1);
   localStorage.removeItem('ListOfBooks');
   localStorage.setItem('ListOfBooks', JSON.stringify(savedData));
-  const checkLi=listCards.querySelector('.shop-list-item')
-    addCard(currentPage);
+  addCard();
 }
 // --------------------Функція створення розмітки картки книги
-function makeMarkup(arr) {
+function makeMarkup11(arr) {
   return arr
     .flatMap(
       ({ book_image, title, list_name, description, author, buy_links, _id }) =>
         `<li class="shop-list-item">
               <svg class="icon-damp" width="28" height="28">
                 <use href="../images/icomoon.svg#icon-dump" id="${_id}"></use>
-              </svg>         
+              </svg>
+         
            <img
              src="${book_image}"
              alt="${title}"
@@ -72,7 +83,8 @@ function makeMarkup(arr) {
              width="100px"
              height: 142px;
              />
-           <div class="info-box">
+         
+          <div class="info-box">
            <h3 class="book-title">${title}</h3>
            <p class="book-category">${list_name}</p>
            <p class="book-deskr" id="style-4">${description}</p>
@@ -96,16 +108,4 @@ function makeMarkup(arr) {
     .join(' ');
 }
 
-function chekIfEmpty() {
-  emptyList.classList.remove('is-hidden');
-  listCards.innerHTML = '';
-  listCards.classList.add('is-hidden');
-  container.classList.add('is-hidden');
-}
 
-function prepareForMarkup() {
-  listCards.classList.remove('is-hidden');
-  container.classList.remove('is-hidden');
-  emptyList.classList.add('is-hidden');
-  listCards.innerHTML = '';
-}
