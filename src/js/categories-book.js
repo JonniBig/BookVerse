e582d9982fce79938c2d372b9a253aee0fd3f4ea
Yevhen-L <ujeen2101@gmail.com>
2023-09-06 10:59:25 +0300

    revert

--- src/js/categories-book.js
+++ src/js/categories-book.js
@@ -17,6 +17,7 @@ async function fetchAndDisplayBooks(categoryName) {
     const words = categoryName.split(' ');
     if (words.length > 1) {
       const lastWord = words.pop();
+
       words.push(`<span style="color: #4F2EE8;">${lastWord}</span>`);
     }
 
@@ -29,78 +30,60 @@ async function fetchAndDisplayBooks(categoryName) {
 
     if (books.length === 0) {
       const noBooksMessage = document.createElement('p');
+
       noBooksMessage.classList.add('categoryesNotFound');
       noBooksMessage.textContent =
         'Вибраної категорії не існує. Виберіть іншу категорію.';
+
       resultContainer.appendChild(noBooksMessage);
     } else {
       const ul = document.createElement('ul');
       ul.className = 'bestseller';
+
+      books.forEach(book => {
+        const li = document.createElement('li');
+        li.className = 'book-item';
+        li.id = book._id;
+
+        const a = document.createElement('a');
+        a.classList.add('best-img-link');
+        a.href = '/book-details.html?id=' + book._id;
+
+        const img = document.createElement('img');
+        img.classList.add('book-wrap');
+        img.src =
+          book.book_image || '../images/plug picture/plug335x485@1x.jpg';
+        img.alt = book.title;
+
+        a.appendChild(img);
+
+        const div2 = document.createElement('div');
+        div2.classList.add('book-info-block');
+
+        const pView = document.createElement('p');
+        pView.classList.add('view');
+        pView.textContent = 'quick view';
+
+        const pTitle = document.createElement('p');
+        pTitle.classList.add('book-name');
+        pTitle.textContent = book.title || 'N/A';
+
+        const pAuthor = document.createElement('p');
+        pAuthor.classList.add('book-author');
+        pAuthor.textContent = book.author || 'N/A';
+
+        div2.appendChild(pView);
+        div2.appendChild(pTitle);
+        div2.appendChild(pAuthor);
+
+        a.appendChild(div2);
+
+        li.appendChild(a);
+        ul.appendChild(li);
+      });
+
       resultContainer.appendChild(ul);
-
-      const intervalDelay = 250; // Затримка між завантаженням кожного елемента (в даному випадку 300 мс)
-
-      async function loadBook(index) {
-        if (index < books.length) {
-          const book = books[index];
-          const li = document.createElement('li');
-          li.className = 'book-item';
-          li.id = book._id;
-          li.style.opacity = '0'; // Початкова непрозорість
-
-          const a = document.createElement('a');
-          a.classList.add('best-img-link');
-          a.href = '/book-details.html?id=' + book._id;
-
-          const img = document.createElement('img');
-          img.classList.add('book-wrap');
-          img.src =
-            book.book_image || '../images/plug picture/plug335x485@1x.jpg';
-          img.alt = book.title;
-
-          const div2 = document.createElement('div');
-          div2.classList.add('book-info-block');
-
-          const pView = document.createElement('p');
-          pView.classList.add('view');
-          pView.textContent = 'view';
-
-          const pTitle = document.createElement('p');
-          pTitle.classList.add('book-name');
-          pTitle.textContent = book.title || 'N/A';
-
-          const pAuthor = document.createElement('p');
-          pAuthor.classList.add('book-author');
-          pAuthor.textContent = book.author || 'N/A';
-
-          div2.appendChild(pView);
-          div2.appendChild(pTitle);
-          div2.appendChild(pAuthor);
-
-          a.appendChild(img);
-          a.appendChild(div2);
-
-          li.appendChild(a);
-          ul.appendChild(li);
-
-          // Прокручуємо сторінку вниз, щоб показати новий елемент
-          window.scrollTo(0, document.body.scrollHeight);
-
-          // Застосовуємо плавну анімацію появи
-          li.style.transition = 'opacity 0.8s';
-          li.style.opacity = '1';
-
-          // Завантажуємо наступний елемент після інтервалу
-          setTimeout(() => {
-            loadBook(index + 1);
-          }, intervalDelay);
-        }
-      }
-
-      // Починаємо завантаження елементів
-      loadBook(0);
     }
-
     const activeCategoryLi = document.querySelector(
       `li[data-category="${categoryName}"]`
     );
@@ -111,5 +94,7 @@ async function fetchAndDisplayBooks(categoryName) {
     console.error('Error fetching books:', error);
   }
 }
+
+// fetchAndDisplayBooks();
 
 export default fetchAndDisplayBooks;
