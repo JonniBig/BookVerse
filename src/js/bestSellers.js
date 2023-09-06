0bd6e7b90ae27a2cef9cabacbf1e5d39fbd4cc18
Svitlo-for <serg_machigin@ukr.net>
2023-09-06 23:14:32 +0300

    icon-book-card

--- src/js/bestSellers.js
+++ src/js/bestSellers.js
@@ -92,10 +92,11 @@ function bindSeeMoreEvent() {
 //--------------------------------
 
 export function renderBook({ book_image, title, author, _id }) {
+  const imageSrc = book_image ? book_image : 'https://raw.githubusercontent.com/JonniBig/BookVerse/main/src/images/plug picture/plug335x485@1x.jpg';
   return `
   <li class="book-item" id="${_id}">
     <a href="" class="best-img-link">
-      <img class="book-wrap" src="${book_image}" alt="${title}" loading="lazy"/>
+      <img class="book-wrap" src="${imageSrc}" alt="${title}" loading="lazy"/>
         <div class="book-info-block">
           <p class="view">quick view</p>
           <p class="book-name">${title}</p>
